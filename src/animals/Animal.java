e70a175427f6ad2ea38e14ce0715e94a20d53ec9
bliminate <blimmer@umich.edu>
2020-09-10 14:55:25 -0600

    Abstract Animal and tests

--- src/animals/Animal.java
+++ src/animals/Animal.java
@@ -1,4 +1,30 @@
 package animals;
 
-public class Animal {
+public abstract class Animal {
+
+   public void sleep(){
+      String output = this.getName() + " the " + this.getType() + " sleeps.";
+      System.out.println(output);
+   }
+
+   public void wake(){
+      String output = this.getName() + " the " + this.getType() + " wakes up.";
+      System.out.println(output);
+   }
+
+   public abstract void makeNoise();
+
+   public void eat(){
+      String output = this.getName() + " the " + this.getType() + " eats.";
+      System.out.println(output);
+   }
+
+   public void roam(){
+      String output = this.getName() + " the " + this.getType() + " roams.";
+      System.out.println(output);
+   }
+
+   public abstract String getName();
+
+   public abstract String getType();
 }
