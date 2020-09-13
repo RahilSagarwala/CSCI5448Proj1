cb2cb6399198879121c153b916a56e2ff4f1ee5d
RahilSagarwala <rasa2575@colorado.edu>
2020-09-13 16:36:07 -0600

    Added Abstract Methods for ZooEmployee
    clean, takeBreak, arriveAtZoo, leaveZoo

--- src/employees/ZooEmployee.java
+++ src/employees/ZooEmployee.java
@@ -1,5 +1,32 @@
 package employees;
 
 public abstract class ZooEmployee {
+	
+	//constructor
+	public ZooEmployee(){
+	      super();
+}
+	
+	public abstract String getName();
+	
+	public void clean () {
+		  String output = this.getName() + "cleans.";
+	      System.out.println(output);
+	}
+	
+	public void takeBreak  () {
+		String output = this.getName() + "takes break.";
+	    System.out.println(output);
+	}
 
+	public void arrivesAtZoo () {
+		String output = this.getName() + "arrives at the zoo.";
+	    System.out.println(output);
+	}
+	
+	public void leaveZoo () {
+		String output = this.getName() + "leaves the zoo.";
+	    System.out.println(output);
+	}
+	
 }
